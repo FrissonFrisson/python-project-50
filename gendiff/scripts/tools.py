c7ac2bc65f54fa6e548b8da16fe74e02c403b3b9
FrissonFrisson <compton56rus@gmail.com>
2023-08-14 21:30:59 +0300

    add diff yaml

--- gendiff/scripts/tools.py
+++ gendiff/scripts/tools.py
@@ -14,14 +14,14 @@ def formating(result):
 
 def load_file(path_1, path_2):
     filename_1, file_ext_1 = os.path.splitext(path_1)
-    filename_2, file_exе_2 = os.path.splitext(path_2)
-    if file_ext_1 == '.json' and file_exе_2 == '.json':
+    filename_2, file_ext_2 = os.path.splitext(path_2)
+    if file_ext_1 == '.json' and file_ext_2 == '.json':
         return json.load(open(path_1)), json.load(open(path_2))
-    elif file_ext_1 == '.yaml' or '.yml' and file_exе_2 == '.yaml' or '.yml':
+    elif file_ext_1 == '.yaml' or '.yml' and file_ext_2 == '.yaml' or '.yml':
         return yaml.safe_load(open(path_1)), yaml.safe_load(open(path_2))
-    elif file_ext_1 == '.yaml' or '.yml' and file_exе_2 == '.json':
+    elif file_ext_1 == '.yaml' or '.yml' and file_ext_2 == '.json':
         return yaml.safe_load(open(path_1)), json.load(open(path_2))
-    elif file_exе_2 == '.yaml' or '.yml' and file_ext_1 == '.json':
+    elif file_ext_2 == '.yaml' or '.yml' and file_ext_1 == '.json':
         return json.load(open(path_1)), yaml.safe_load(open(path_2))
     else:
         return
