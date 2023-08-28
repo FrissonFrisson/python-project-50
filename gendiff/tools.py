b6db9f0ac8d6435404596f02ffdb3e0f2271c30b
kali <kali@kali.myguest.virtualbox.org>
2023-08-28 21:46:52 +0300

    s

--- gendiff/tools.py
+++ gendiff/tools.py
@@ -43,32 +43,54 @@ def parsing(path_1, path_2):
     
 
 def formating(result, symb=' ', count=1):
-    def inner(curent_value, depth=1):
+    def inner(curent_value, depth=0):
         result = []
         case_symb = symb*depth*count
         for key, value in curent_value.items():
-            if value == "missing_file_1":
-                result.append(case_symb + '+ ' + key)
-            elif value == "missing_file_2":
-                result.append(case_symb + '- ' + key)
-            elif value == "equal":
-                result.append(case_symb + key)
-            elif isinstance(value, dict) and not "equal" in key:
-                    result.append(case_symb + inner(value, depth + 1))
-            elif "equal" in key:
-                result.append(case_symb + key[0] + " {")
-                if isinstance(value, dict):
-                    result.append(case_symb + inner(value, depth + 1))
-                elif isinstance(value, tuple):
-                    value_1, value_2 = value
-                    result.append(case_symb + '- ' + value_1)
-                    result.append(case_symb + '+ ' + value_1)
-                result.append(case_symb+'}')
-            elif isinstance(key, tuple) and value == "different value":
-                    value_1, value_2 = key
-                    result.append(case_symb + '- ' + value_1)
-                    result.append(case_symb + '+ ' + value_1)
-                
+            if "missing_file_1" in value:
+                result.append(f'{case_symb}+ {key}: {value[0]}')
+            elif "missing_file_2" in value:
+                result.append(f'{case_symb}- {key}: {value[0]}')
+            elif "equal" in value:
+                result.append(f'{case_symb}  {key}: {value[0]}')
+            elif "different value" in value:
+                result.append(f'{case_symb}- {key}: {value[0]}')
+                result.append(f'{case_symb}+ {key}: {value[1]}')
+            elif isinstance(value, dict):
+                nested_diff = case_symb + inner(value, depth + 1)+'\n' + case_symb + '}'
+                result.append(f'{case_symb}{key}: {{')
+                result.append(nested_diff)
+
+def format_data(data):
+    result = "{\n"
+    indent = "  "
+
+    def format_value(value):
+        if isinstance(value, dict):
+            return format_dict(value)
+        elif isinstance(value, tuple):
+            if len(value) == 2:
+                return f"{value[0]}: {value[1]}"
+            elif len(value) == 3:
+                return f"{value[0]}: {value[1]} ({value[2]})"
+        else:
+            return str(value)
+
+    def format_dict(dictionary):
+        inner_result = ""
+        for key, value in dictionary.items():
+            if isinstance(value, dict):
+                inner_result += f"{indent}{key}: {format_dict(value)}{indent}{indent}}}\n"
+            else:
+                inner_result += f"{indent}{key}: {format_value(value)}\n"
+        return inner_result
+
+    for key, value in data.items():
+        result += f"{indent}{key}: {format_dict(value)}{indent}}}\n"
+
+    result += "}"
+    return result
+
         return '\n'.join(result)
     return inner(result)
                 
@@ -76,23 +98,19 @@ def formating(result, symb=' ', count=1):
 
 
 def find_differences(file_1, file_2):
-    def inner(data_1, data_2):
-        result = {}
-        keys = sorted(data_1.keys() | data_2.keys())
-        for key in keys:
-            value_1, value_2 = data_1.get(key), data_2.get(key)
-            string_1, string_2 = f'{key}: {value_1}', f'{key}: {value_2}'
-            if key not in data_1:
-                
-                result[string_2] = "missing_file_1"
-            elif key not in data_2:
-                result[string_1] = "missing_file_2"
-            elif value_1 == value_2:
-                result[(string_1)] = "equal"
+    result = {}
+    keys = sorted(file_1.keys() | file_2.keys())
+    for key in keys:
+        value_1, value_2 = file_1.get(key), file_2.get(key)
+        if key not in file_1:
+            result[key] = (value_2, "missing_file_1")
+        elif key not in file_2:
+            result[key] = (value_1, "missing_file_2")
+        elif value_1 == value_2:
+            result[key] = (value_1, 'equal')
+        else:
+            if isinstance(value_1, dict) and isinstance(value_2, dict):
+                result[key] = (find_differences(value_1, value_2))
             else:
-                if isinstance(value_1, dict) and isinstance(value_2, dict):
-                    result[key+':', "equal"] = inner(value_1, value_2)
-                else:
-                    result[(string_1, string_2)] = "different value"
-        return result
-    return formating(inner(file_1, file_2))
+                result[key] = (value_1, value_2, "different value")
+    return result
