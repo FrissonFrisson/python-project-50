96cfd52f1f15da071aea69de378729c50337ab67
kali <kali@kali.myguest.virtualbox.org>
2023-08-29 21:47:15 +0300

    formating value

--- gendiff/tools.py
+++ gendiff/tools.py
@@ -4,28 +4,6 @@ import yaml
 import os
 
 
-def stringify(data, symb=' ', count=1,):
-    def iter_(curent_value, depth=1):
-        if not isinstance(curent_value, (list, tuple, set, dict)):
-            return str(curent_value)
-        result = []
-        case_symb = symb*depth*count
-        if isinstance(curent_value, (dict)):
-            result.append('{')
-            for key, value in curent_value.items():
-                result.append(f'{case_symb}{key}: {iter_(curent_value[key], depth + 1)}')
-        result.append(symb*(depth-1)*count+'}')
-        return '\n'.join(result)
-    return iter_(data, depth=1)
-def formating_value(string):
-    string = str(string)
-    string = '{\n'+'\n'.join(result)+'\n}'
-    string = string.replace('True', 'true')
-    string = string.replace('False', 'false')
-    string = string.replace('None', 'null')
-    return string
-
-
 def parsing(path_1, path_2):
     filename_1, file_ext_1 = os.path.splitext(path_1)
     filename_2, file_ext_2 = os.path.splitext(path_2)
@@ -40,61 +18,50 @@ def parsing(path_1, path_2):
     elif file_ext_2 in ['.yaml', '.yml']:
         file_2 = yaml.safe_load(open(path_2))
     return file_1, file_2
-    
 
-def formating(result, symb=' ', count=1):
-    def inner(curent_value, depth=0):
-        result = []
-        case_symb = symb*depth*count
-        for key, value in curent_value.items():
-            if "missing_file_1" in value:
-                result.append(f'{case_symb}+ {key}: {value[0]}')
-            elif "missing_file_2" in value:
-                result.append(f'{case_symb}- {key}: {value[0]}')
-            elif "equal" in value:
-                result.append(f'{case_symb}  {key}: {value[0]}')
-            elif "different value" in value:
-                result.append(f'{case_symb}- {key}: {value[0]}')
-                result.append(f'{case_symb}+ {key}: {value[1]}')
-            elif isinstance(value, dict):
-                nested_diff = case_symb + inner(value, depth + 1)+'\n' + case_symb + '}'
-                result.append(f'{case_symb}{key}: {{')
-                result.append(nested_diff)
 
-def format_data(data):
-    result = "{\n"
-    indent = "  "
+def formating_value(string, indent):
+    string = string.replace('True', 'true')
+    string = string.replace('False', 'false')
+    string = string.replace('None', 'null')
+    return string
 
-    def format_value(value):
+def formating_children(d, symb='    ', count=1, depth=1):
+    result = []
+    indent = symb * count * depth
+    for key, value in d.items():
         if isinstance(value, dict):
-            return format_dict(value)
-        elif isinstance(value, tuple):
-            if len(value) == 2:
-                return f"{value[0]}: {value[1]}"
-            elif len(value) == 3:
-                return f"{value[0]}: {value[1]} ({value[2]})"
+            result.append(formating_children(value, symb, count, depth+1))
         else:
-            return str(value)
+            result.append(f'{indent}{key} : {value}')
+    return '\n'.join(result)
 
-    def format_dict(dictionary):
-        inner_result = ""
-        for key, value in dictionary.items():
-            if isinstance(value, dict):
-                inner_result += f"{indent}{key}: {format_dict(value)}{indent}{indent}}}\n"
+def formating(data, symb='    ', count=1, depth=1):
+    result = []
+    indent = symb * count * depth
+
+    for groups, value in data.items():
+        if isinstance(value, dict):
+            symb_diff = '  '
+            result.append(f'{indent}{symb_diff}{groups}: {{')
+            result.append(formating(value, symb, count, depth + 1))
+            result.append(f'{indent}{symb_diff}}}')
+        else:
+            if isinstance(value[0], dict):
+                value = (formating_children(value[0], symb='    ', count=1, depth=1), *value[1:])
+
+            symb_diff = ' '
+            if value[1] == "missing_file_1":
+                symb_diff = '+'
+            elif value[1] == "missing_file_2":
+                symb_diff = '-'
+            if "different value" not in value:
+                result.append(f'{indent}{symb_diff} {groups}: {value[0]}')
             else:
-                inner_result += f"{indent}{key}: {format_value(value)}\n"
-        return inner_result
+                result.append(f'{indent}- {groups}: {value[0]}')
+                result.append(f'{indent}+ {groups}: {value[1]}')
 
-    for key, value in data.items():
-        result += f"{indent}{key}: {format_dict(value)}{indent}}}\n"
-
-    result += "}"
-    return result
-
-        return '\n'.join(result)
-    return inner(result)
-                
-
+    return formating_value('\n'.join(result), indent)
 
 
 def find_differences(file_1, file_2):
@@ -108,9 +75,9 @@ def find_differences(file_1, file_2):
             result[key] = (value_1, "missing_file_2")
         elif value_1 == value_2:
             result[key] = (value_1, 'equal')
+        elif isinstance(value_1, dict) and isinstance(value_2, dict):
+            result[key] = (find_differences(value_1, value_2))
         else:
-            if isinstance(value_1, dict) and isinstance(value_2, dict):
-                result[key] = (find_differences(value_1, value_2))
-            else:
-                result[key] = (value_1, value_2, "different value")
+            result[key] = (value_1, value_2, "different value")
+    print(result)
     return result
