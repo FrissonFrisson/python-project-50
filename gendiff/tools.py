c90930b78a72fe75184ca184f12976fbd3347402
kali <kali@kali.myguest.virtualbox.org>
2023-08-30 20:21:41 +0300

    formating

--- gendiff/tools.py
+++ gendiff/tools.py
@@ -20,21 +20,22 @@ def parsing(path_1, path_2):
     return file_1, file_2
 
 
-def formating_value(string, indent):
+def formating_value(string):
     string = string.replace('True', 'true')
     string = string.replace('False', 'false')
     string = string.replace('None', 'null')
     return string
 
-def formating_children(d, symb='    ', count=1, depth=1):
+def formating_child(d, curent_indent = '', symb='    ', count=1, depth=1):
     result = []
-    indent = symb * count * depth
+    indent = (symb * count * depth)+curent_indent
     for key, value in d.items():
         if isinstance(value, dict):
-            result.append(formating_children(value, symb, count, depth+1))
-        else:
-            result.append(f'\n{{{indent}{key} : {value}')
+            result.append(indent+key+': {')
+            result.append(formating_child(value, curent_indent, depth=depth+1))
             result.append(indent+'}')
+        else:
+            result.append(f'{indent}{key}: {value}')
     return '\n'.join(result)
 
 def formating(data, symb='    ', count=1, depth=1):
@@ -42,27 +43,25 @@ def formating(data, symb='    ', count=1, depth=1):
     indent = symb * count * depth
 
     for groups, value in data.items():
+        symb_diff = '  '
         if isinstance(value, dict):
-            symb_diff = '  '
-            result.append(f'{indent}{symb_diff}{groups}: {{')
+            result.append(f'{indent}{symb_diff}{groups}: ')
             result.append(formating(value, symb, count, depth + 1))
-            result.append(f'{indent}{symb_diff}}}')
         else:
             if isinstance(value[0], dict):
-                value = (formating_children(value[0], symb='    ', count=1, depth=1), *value[1:])
-
-            symb_diff = ' '
-            if value[1] == "missing_file_1":
-                symb_diff = '+'
-            elif value[1] == "missing_file_2":
-                symb_diff = '-'
+                curent_indent = indent+symb_diff
+                child = formating_child(value[0], curent_indent , symb, count) 
+                value = ('{\n' + child + '\n' + curent_indent +'}', *value[1:])
+            symb_diff = '+ ' if value[1] == "missing_file_1" else symb_diff
+            symb_diff = '- ' if value[1] == "missing_file_2" else symb_diff
             if "different value" not in value:
-                result.append(f'{indent}{symb_diff} {groups}: {value[0]}')
+                result.append(f'{indent}{symb_diff}{groups}: {value[0]}')
             else:
                 result.append(f'{indent}- {groups}: {value[0]}')
                 result.append(f'{indent}+ {groups}: {value[1]}')
 
-    return formating_value('\n'.join(result), indent)
+
+    return formating_value('\n'.join(result))
 
 
 def find_differences(file_1, file_2):
