0bc2d5a4ad6ab6f446ef793a93ce87808b78866e
FrissonFrisson <compton56rus@gmail.com>
2023-09-03 23:41:50 +0300

    add format plain

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -11,14 +11,17 @@ def main():
     parser.add_argument('second_file', type=str, help='''
     Path to the second file''')
     parser.add_argument("-f", "--format", type=str, help='''
-    set format of output''')
+    set format of output: stylish. plain or json''')
     args = parser.parse_args()
-    print(gen_diff(args.first_file, args.second_file))
+    print(gen_diff(args.first_file, args.second_file, args.format))
 
 
-def gen_diff(path_file_1, path_file_2):
+def gen_diff(path_file_1, path_file_2, format_name):
     file_1, file_2 = tools.parsing(path_file_1, path_file_2)
-    return tools.format_stylish(tools.find_differences(file_1, file_2))
+    diff = tools.find_differences(file_1, file_2)
+    if format_name == "plain":
+        return tools.format_plain(diff)
+    return tools.format_stylish(diff)
 
 
 if __name__ == '__main__':
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -7,16 +7,16 @@ def test_gen_diff():
     file_1 = 'tests/fixtures/fixture_1.json'
     file_2 = 'tests/fixtures/fixture_2.json'
     result = open('tests/fixtures/fixture_result_1x2.txt', "r").read()
-    assert gen_diff(file_1, file_2) == result
+    assert gen_diff(file_1, file_2, 'stylish') == result
     file_1 = 'tests/fixtures/fixture_3.yaml'
     file_2 = 'tests/fixtures/fixture_4.yml'
     result = open('tests/fixtures/fixture_result_3x4.txt', "r").read()
-    assert gen_diff(file_1, file_2) == result
+    assert gen_diff(file_1, file_2, 'stylish') == result
     file_1 = 'tests/fixtures/fixture_3.yaml'
     file_2 = 'tests/fixtures/fixture_2.json'
     result = open('tests/fixtures/fixture_result_3x2.txt', "r").read()
-    assert gen_diff(file_1, file_2) == result
+    assert gen_diff(file_1, file_2, 'stylish') == result
     file_1 = 'tests/fixtures/fixture_5.yml'
     file_2 = 'tests/fixtures/fixture_6.json'
     result = open('tests/fixtures/fixture_result_5x6.txt', "r").read()
-    assert gen_diff(file_1, file_2) == result
+    assert gen_diff(file_1, file_2, 'stylish') == result
